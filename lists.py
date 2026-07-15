0ab12281e781d28ddf8cff49ef055fc9bcf2d17e
DenizShayan <92824050+DenizShayan@users.noreply.github.com>
2026-07-14 22:53:52 -0400

    Create lists.py
    Practice Python list operations using indexing, append(), and sort().

---
+++ lists.py
@@ -0,0 +1,11 @@
+# define a list of names
+names = ["Harry", "Ron", "Hermione"]
+
+print(names[0])  # prints "Harry"
+
+names.append("Draco")  # adds "Draco" to the end of the list
+
+names.sort()
+
+print(names)  # prints the sorted list of names
+
