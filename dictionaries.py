1ac17ce4e0e4c9a14ee6418e741c3609c110d429
DenizShayan <92824050+DenizShayan@users.noreply.github.com>
2026-07-22 22:06:19 -0400

    Add dictionary entry
    Add Hermione to dictionary

--- dictionaries.py
+++ dictionaries.py
@@ -1,5 +1,7 @@
 houses = {"Harry": "Gryffindor", "Draco": "Slytherin", "Luna": "Ravenclaw", "Cedric": "Hufflepuff"}
 
+houses["Hermione"] = "Gryffindor"  # Adding a new key-value pair
+
 print(houses["Harry"])  # Output: Gryffindor
 
 
