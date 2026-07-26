2161b99e705cc5c75bb997d344ad1385310a1e2d
DenizShayan <92824050+DenizShayan@users.noreply.github.com>
2026-07-26 14:24:48 -0400

    Extend functions module with cube()
    Add the cube() function to the functions module and demonstrate its usage in squares.py.

--- functions.py
+++ functions.py
@@ -1,3 +1,6 @@
 def square(x):
     """Returns the square of a number."""
     return x * x
+
+def cube(x):
+    return x ** 3
--- squares.py
+++ squares.py
@@ -2,3 +2,4 @@ import functions
 
 for i in range(10):
     print(f"The square of {i} is {functions.square(i)}")
+    print(f"The cube of {i} is {functions.cube(i)}")
