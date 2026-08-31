4acc69b6d571dee0be875c4de08827958a3a40f2
DenizShayan <92824050+DenizShayan@users.noreply.github.com>
2026-08-31 02:04:37 -0400

    Add dynamic greeting route
    Allow the greet view to accept any name from the URL.
    By adding any name to the end of the URL, the app will display "Hello" followed by that name!!!

--- cs50w_django/hello/urls.py
+++ cs50w_django/hello/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("<str:name>", views.greet, name="greet"),
     path("deniz", views.deniz, name="deniz"),
     path("david", views.david, name="david")
 ]
