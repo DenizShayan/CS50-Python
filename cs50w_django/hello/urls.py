9bb3a93c37746e8355d8e42cea1f29a35ebda9ce
DenizShayan <92824050+DenizShayan@users.noreply.github.com>
2026-08-30 22:32:56 -0400

    add deniz and david routes
    Added URL paths for deniz and david views.

--- cs50w_django/hello/urls.py
+++ cs50w_django/hello/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 
+from . import views
+
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("deniz", views.deniz, name="deniz"),
+    path("david", views.david, name="david")
 ]
